625b7321d251a58715e4b401d0182263394ac45b
Debmalya Roy <arghya_roy@yahoo.com>
2019-12-08 09:24:32 +0530

    v0.0.2

--- company-portal/src/main/java/com/LTI/companyportal/CompanyPortalApplication.java
+++ company-portal/src/main/java/com/LTI/companyportal/CompanyPortalApplication.java
@@ -14,9 +14,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDbFactory;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import com.LTI.companyportal.models.Employee;
+import com.LTI.companyportal.models.JmsPublisherApplication;
 import com.LTI.companyportal.repositories.EmployeeRepository;
+import com.mongodb.client.MongoClients;
+
+
+import org.springframework.data.mongodb.core.MongoOperations;
 
 @RestController
 @RequestMapping("/employee")
@@ -24,23 +33,48 @@ import com.LTI.companyportal.repositories.EmployeeRepository;
 
 @SpringBootApplication
 @ComponentScan
-@EnableAutoConfiguration
-@EnableMongoRepositories
+//@EnableAutoConfiguration
+//@EnableMongoRepositories
 
 public class CompanyPortalApplication {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
+	SimpleMongoClientDbFactory mongoDBFactory = new SimpleMongoClientDbFactory("mongodb://localhost:47017/rest_tutorial");
+	Query query1 = new Query();
+	MongoOperations mongoOps = new MongoTemplate(mongoDBFactory);
+	
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
 	public Employee getEmployeeById(@PathVariable("id") ObjectId id) {
 		return employeeRepository.findBy_id(id);
 	}
+	
+	
 	@RequestMapping(value = "/add", method = RequestMethod.PUT)
 	public void modifyEmployeeById(@Valid @RequestBody Employee employee){
 		//employee.set_id(ObjectId.get());		
 		employeeRepository.save(employee);
 	}
+	
+	@RequestMapping(value = "/name/{name}", method = RequestMethod.GET)
+	public String getEmployeeByName(@PathVariable("name") String name){
+		query1.addCriteria(Criteria.where("name").is(name));
+		Employee emp = mongoOps.findOne(query1, Employee.class);
+		return "Employee Details: " + "Name: " + emp.getName() + " Address: " + emp.getAddress()
+		+ " PAN: " + emp.getPan() + " Package: " + emp.getAnnual_package() + " E-Mail: " + emp.getEmail();
+	}
+	
+	@RequestMapping(value="/pushtds/{name}", method = RequestMethod.GET)
+	public void sendEmpDetails(@PathVariable("name") String name) {
+		
+		query1.addCriteria(Criteria.where("name").is(name));
 
+		Employee emp = mongoOps.findOne(query1, Employee.class);
+		System.out.println(emp.toString());
+		JmsPublisherApplication jmsPub = new JmsPublisherApplication();
+		jmsPub.sendMessage(emp);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CompanyPortalApplication.class, args);
 	}
--- company-portal/src/main/java/com/LTI/companyportal/repositories/EmployeeRepository.java
+++ company-portal/src/main/java/com/LTI/companyportal/repositories/EmployeeRepository.java
@@ -1,10 +1,15 @@
 package com.LTI.companyportal.repositories;
 
 import org.bson.types.ObjectId;
+//import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.repository.MongoRepository;
+//import org.springframework.stereotype.Component;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.mongodb.core.MongoOperations;
 
 import com.LTI.companyportal.models.Employee;
 
 public interface EmployeeRepository extends MongoRepository<Employee, String> {
 	Employee findBy_id(ObjectId _id);
+	//Employee findByName(String name);
 }
